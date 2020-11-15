06939b8aee4ad6b602213a66869ae4d257263cc8
NohYeaJin <38969669+NohYeaJin@users.noreply.github.com>
2020-11-15 20:58:25 +0900

    add

--- djangoProject/MacKiosk/models.py
+++ djangoProject/MacKiosk/models.py
@@ -5,24 +5,25 @@ from django.db import models
 
 class Menus(models.Model):
 
-    MenuName = models.CharField('MenuName',max_length=50)
+    MenuName = models.CharField('MenuName',max_length=50,primary_key=True)
     MenuPrice = models.IntegerField('MenuPrice')
 
     def __str__(self):
-        return self.MenuName
+        return [self.MenuName, self.MenuPrice]
 
     class Meta:
         managed = False
         db_table = 'menus'
 
-
-
 class Orders(models.Model):
 
-    OrderNum = models.IntegerField('OrderNum')
+    OrderNum = models.IntegerField('OrderNum',primary_key=True)
     OrderQty = models.IntegerField('OrderQty')
-    MenuName = models.CharField('OrderMenu', max_length=50)
+    OrderMenu = models.CharField('OrderMenu', max_length=50)
     OrderDate = models.DateField('OrderDate')
+
+    def __str__(self):
+        return [self.OrderNum, self.OrderQty,self.OrderMenu,self.OrderDate]
 
     def get_order_num(self):
         return self.OrderNum
@@ -35,6 +36,10 @@ class Orders(models.Model):
 
     def get_order_date(self):
         return self.OrderDate
+
+    class Meta:
+        managed = False
+        db_table = 'orders'
 
 
 
