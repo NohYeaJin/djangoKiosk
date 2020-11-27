651b83b7857ec3ebe1aad7efc7b7de693de96f02
Jin-R <54504883+Jin-R@users.noreply.github.com>
2020-11-27 13:20:06 +0900

    path related to app is moved  from djangoProject

---
+++ djangoProject/MacKiosk/urls.py
@@ -0,0 +1,43 @@
+"""MacKiosk URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from .views import *
+
+app_name = 'MacKiosk'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('kiosk/menus/', menuSelect, name='menuSelect'),
+    path('kiosk/quantity/', quantitySelect, name='quantitySelect'),
+    path('kiosk/addCart/', addCart, name='addCart'),
+    path('kiosk/toCart/', toCart.as_view(), name='toCart'),
+    path('kiosk/payment/', payment, name='payment'),
+    path('kiosk/reset/', reset, name='reset'),
+    path('kiosk/chQty/', chQty, name='chQty'),
+    path('kiosk/cancleMenu/', cancelMenu, name='cancleMenu'),
+    path('kiosk/complete/', complete, name='complete'),
+    path('kiosk/showOrderNum/', showOrderNum, name='showOrderNum'),
+
+    path('seller/postmenu/', postmenu, name='postmenu'),
+    path('seller/callList/', callList.as_view(), name='callList'),
+    path('sellet/orderList/', orderList.as_view(), name='orderList'),
+    path('seller/callCustomer/', callCustomer, name='callCustomer'),
+    path('seller/menuComplete/', menuComplete, name='menuComplete'),
+    path('seller/cancelOrder/', cancelOrder, name='cancelOrder'),
+    path('seller/orderIngrd/', orderIngrd, name='orderIngrd'),
+    path('seller/srchIngrd/', srchIngrd.as_view(), name='srchIngrd'),
+    path('seller/srchTerm/', srchTerm.as_view(), name='srchTerm'),
+]
